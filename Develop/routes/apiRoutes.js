f38e6baa74e3ef996641a7c429ec0c7c164a15f0
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-06-18 11:13:15 -0400

    added api routes to apiRoutes file.

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const fs = require('fs');
+//const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const db = '../develop/db/db.json';
+
+
+  // GET /api/notes route
+router.get('/api/notes', (req, res) => {
+  fs.readFile(db, 'utf8', (err, data) => res.json(JSON.parse(data)))
+});
+
+  // POST /api/notes route
+router.post('/api/notes', (req, res) => {
+  
+  const { title, text } = req.body; 
+  console.log(req.body);
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      newNote_id: uuidv4()
+    };
+
+    fs.readFile(db, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        let parsedData = JSON.parse(data);
+        if (!Array.isArray(parsedData)) {
+          parsedData = [];
+    }
+        parsedData.push(newNote);
+        fs.writeFile(db, JSON.stringify(parsedData, null, 4), (err) =>
+          err ? console.error(err) : console.info(`\nData written to ${db}`)
+        );
+      }
+    })
+
+      const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.status(200).send(response);
+  } else {
+    res.status(400).send('Error in adding note');
+  }
+
+});
+
+
+module.exports = router;
